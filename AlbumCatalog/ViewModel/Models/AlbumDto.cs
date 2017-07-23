4bddc8c732d81610184a1337fa503139f6ec9368
lexa040494 <lexa040494@mail.ru>
2017-07-23 19:03:11 +0300

    Add view model

---
+++ AlbumCatalog/ViewModel/Models/AlbumDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Models
+{
+    [DataContract]
+    public class AlbumDto
+    {
+        [DataMember]
+        public decimal Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int  Year { get; set; }
+        [DataMember]
+        public List<TrackDto> Tracks { get; set; } 
+    }
+}
---
+++ AlbumCatalog/ViewModel/Models/TrackDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Models
+{
+    [DataContract]
+    public class TrackDto
+    {
+        [DataMember]
+        public decimal Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Artist { get; set; }
+        [DataMember]
+        public string Duration { get; set; }
+        [DataMember]
+        public decimal AlbumId { get; set; } 
+    }
+}
